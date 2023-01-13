80a5a68e6dc585ad98f3f2e461caca4374070857
Rafael B. Januzi <rjanuzi@gmail.com>
2023-01-13 09:31:26 -0300

    7DaysOfCode - MongoDB - Refactoring code

--- db-connection.js
+++ db-connection.js
@@ -1,32 +1,33 @@
-const { MongoClient } = require('mongodb')
-const dbConfig = require('./db-config.json')
-console.log(dbConfig)
-const uri =
-  'mongodb+srv://' +
-  dbConfig.username +
-  ':' +
-  dbConfig.password +
-  '@' +
-  dbConfig.host +
-  '/' +
-  dbConfig.db
+import mongodb from "mongodb";
+import fs from "fs";
 
-const client = new MongoClient(uri)
+const DB_CONFIG_PATH = "./db-config.json";
 
-async function run () {
+function getMongoDbUri() {
+  const dbConfig = JSON.parse(fs.readFileSync(DB_CONFIG_PATH));
+  return `mongodb+srv://${dbConfig.username}:${dbConfig.password}@${dbConfig.host}/${dbConfig.db}?retryWrites=true&w=majority`;
+}
+
+function getMongoDbName() {
+  const dbConfig = JSON.parse(fs.readFileSync(DB_CONFIG_PATH));
+  return dbConfig.db;
+}
+
+const uri = getMongoDbUri();
+const dbName = getMongoDbName();
+const client = new mongodb.MongoClient(uri);
+
+export default async function findAll(collectionName) {
   try {
-    const database = client.db(dbConfig.db)
-    const movies = database.collection('characters')
+    const database = client.db(dbName);
+    const dbCollection = database.collection(collectionName);
 
-    // query for movies that have a runtime less than 15 minutes
-    const query = {}
-
-    const cursor = movies.find(query)
+    const query = {}; // empty query to find all documents
+    const cursor = dbCollection.find(query);
 
     // replace console.dir with your callback to access individual elements
-    await cursor.forEach(console.dir)
+    return await cursor.toArray();
   } finally {
-    await client.close()
+    await client.close();
   }
 }
-run().catch(console.dir)
--- index.js
+++ index.js
@@ -1,11 +1,19 @@
-const express = require('express')
-const app = express()
-const port = 80
+import express from "express";
+import findAll from "./db-connection.js";
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+const app = express();
+const port = 80;
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.get("/characters", (req, res) => {
+  findAll("characters")
+    .then((tuples) => res.send(tuples))
+    .catch((e) => res.send(e));
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening on  http://localhost:${port}`)
-})
+  console.log(`Example app listening on  http://localhost:${port}`);
+});
